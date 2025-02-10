e272604e6df0adfb86518c0ffdbc1a8451f4005d
Julian Meyer <julian.meyer@coinbase.com>
2025-02-10 10:42:43 -0800

    Don't overwrite alloc

--- op-e2e/actions/upgrades/isthmus_fork_test.go
+++ op-e2e/actions/upgrades/isthmus_fork_test.go
@@ -494,7 +494,7 @@ func TestSetCodeTxTypeIsthmus(gt *testing.T) {
 	store42Program := program.New().Sstore(0x42, 0x42)
 	callBobProgram := program.New().Call(nil, dp.Addresses.Bob, 1, 0, 0, 0, 0)
 
-	alloc := helpers.DefaultAlloc
+	alloc := *helpers.DefaultAlloc
 	alloc.L2Alloc = make(map[common.Address]types.Account)
 	alloc.L2Alloc[aa] = types.Account{
 		Code: store42Program.Bytes(),
@@ -503,7 +503,7 @@ func TestSetCodeTxTypeIsthmus(gt *testing.T) {
 		Code: callBobProgram.Bytes(),
 	}
 
-	sd := e2eutils.Setup(t, dp, alloc)
+	sd := e2eutils.Setup(t, dp, &alloc)
 	log := testlog.Logger(t, log.LevelError)
 	miner, seqEngine, sequencer := helpers.SetupSequencerTest(t, sd, log)
 	_, verifier := helpers.SetupVerifier(t, sd, log, miner.L1Client(t, sd.RollupCfg), miner.BlobStore(), &sync.Config{})
