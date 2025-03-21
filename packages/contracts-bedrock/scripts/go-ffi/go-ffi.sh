ac78cc76a91d6206cc570a019e77cf8bb0130ad6
Maurelian <maurelian@protonmail.ch>
2025-03-21 13:13:32 -0400

    feat: Use make to dynamically build go-ffi binaries only if needed

--- packages/contracts-bedrock/scripts/go-ffi/go-ffi.sh
+++ packages/contracts-bedrock/scripts/go-ffi/go-ffi.sh
@@ -1,40 +1,18 @@
 #!/bin/bash
 
-{
-    # Define a lock file
-    LOCKFILE="/tmp/go-ffi.lock"
+set -euo pipefail # Fail on errors, undefined vars, and pipe failures
 
-    # Function to clean up the lock file on exit
-    cleanup() {
-        rm -rf "$LOCKFILE"
-    }
+# Get the directory of the script
+dir=$(dirname "$(realpath "$0")")
 
-    # Get the directory of the script
-    dir=$(dirname "$(realpath "$0")")
-    # Check if the target binary exists
-    if [[ ! -f "$dir/$1" ]]; then
-        # Wait for the lock file to be released
-        while ! mkdir "$LOCKFILE" > /dev/null; do
-            sleep 0.3s
-        done
-
-        # Set a trap to call cleanup on exit
-        trap cleanup EXIT
-
-        # Ensure only one process is building the binaries
-        if [[ ! -f "$dir/$1" ]]; then
-            pushd "$dir/../.."
-            just build-go-ffi > /dev/null
-            popd
-        fi
-        cleanup
-        # Remove the trap since it's no longer needed
-        trap - EXIT
-    fi
-} > /dev/null 2>&1 # Ensures nothing is printed to the console during the build process
-
-
+# Get the target binary name from the first argument
 target_binary="$dir/$1"
-shift  # This removes the first argument
+
+# Build the binaries if needed
+make go-ffi > /dev/null 2>&1
+
+# Remove the first argument
+shift
+
 # Call the target binary with remaining arguments
 $target_binary "$@"
