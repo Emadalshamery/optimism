97dc5015658ba6329fd51f9b3643db8f16731615
Michael Amadi <amadimichaeld@gmail.com>
2025-04-17 12:56:45 +0100

    add semgrep rule to assert no usage of testFail

---
+++ .semgrep/tests/sol-rules.sol-style-no-testFail-usage.sol
@@ -0,0 +1,13 @@
+contract SemgrepTest__sol_style_no_testFail_usage {
+    // ruleid: sol-style-no-testFail-usage
+    function testFail() public {}
+
+    // ruleid: sol-style-no-testFail-usage
+    function testFail2() public {}
+
+    // ok: sol-style-no-testFail-usage
+    function testPass() public {}
+
+    // ok: sol-style-no-testFail-usage
+    function testPass2() public {}
+}
