841701144d93dbaa781520c13c3c9493d4e0de4d
inphi <mlaw2501@gmail.com>
2025-05-05 15:36:22 -0400

    fix asr script

--- packages/contracts-bedrock/scripts/asr_initialize.s.sol
+++ packages/contracts-bedrock/scripts/asr_initialize.s.sol
@@ -8,19 +8,26 @@ import { AnchorStateRegistry } from "src/dispute/AnchorStateRegistry.sol";
 import { Enum } from "safe-contracts/common/Enum.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { Vm } from "forge-std/Vm.sol";
+import { IDisputeGameFactory } from "interfaces/dispute/IDisputeGameFactory.sol";
+import { ISystemConfig } from "interfaces/L1/ISystemConfig.sol";
+import { GameType, Proposal, Hash } from "src/dispute/lib/Types.sol";
 
 contract ASRInitialize is Script {
     function run() external view {
         string memory jsonStr = vm.readFile("asr_initialize_input.json");
         address targetAddress = vm.envAddress("TARGET_ADDRESS");
-        address systemConfigProxy = stdJson.readAddress(jsonStr, ".systemConfigProxy");
-        address dgfProxy = stdJson.readAddress(jsonStr, ".disputeGameFactoryProxy");
-        bytes32 outputRoot = stdJson.readBytes32(jsonStr, ".outputRoot");
+        ISystemConfig systemConfigProxy = ISystemConfig(stdJson.readAddress(jsonStr, ".systemConfigProxy"));
+        IDisputeGameFactory dgfProxy = IDisputeGameFactory(stdJson.readAddress(jsonStr, ".disputeGameFactoryProxy"));
+        Hash outputRoot = Hash.wrap(stdJson.readBytes32(jsonStr, ".outputRoot"));
         uint256 blockNumber = stdJson.readUint(jsonStr, ".blockNumber");
-        uint32 gameType = stdJson.readUint(jsonStr, ".gameType");
+        uint32 gameType = uint32(stdJson.readUint(jsonStr, ".gameType"));
+        Proposal memory proposal = Proposal({
+            root: outputRoot,
+            l2SequenceNumber: blockNumber
+        });
 
         bytes memory data = abi.encodeCall(
-            AnchorStateRegistry.initialize, (systemConfigProxy, dgfProxy, (outputRoot, blockNumber), gameType)
+            AnchorStateRegistry.initialize, (systemConfigProxy, dgfProxy, proposal, GameType.wrap(gameType))
         );
 
         bytes memory safeCalldata = abi.encodeCall(
